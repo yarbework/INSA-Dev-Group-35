52b4c70245952113398fa27dceb54b0495ccfb40
Yarbework <yarbemohaz3517@gmail.com>
2025-08-03 21:14:40 +0300

    adding tailwind css and react router

--- FrontEnd/src/Components/ErrorMessage.tsx
+++ FrontEnd/src/Components/ErrorMessage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 
 function ErrorMessage() {
   return <div>ErrorMessage</div>;
--- FrontEnd/src/vite-env.d.ts
+++ FrontEnd/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss()],
+});
