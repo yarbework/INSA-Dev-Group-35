f4b9b36cbc8610ff072ad10b1daae30968c5bd4c
Yarbework <117997672+yarbework@users.noreply.github.com>
2025-08-03 16:59:36 +0300

    Delete QuizGeek/FrontEnd/src directory

--- QuizGeek/FrontEnd/src/Pages/AboutUs.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AboutUs() {
-  return <div>About Us Page</div>;
-}
-
-export default AboutUs;
--- QuizGeek/FrontEnd/src/Pages/Error.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Error() {
-  return <div>Error Loading</div>;
-}
-
-export default Error;
