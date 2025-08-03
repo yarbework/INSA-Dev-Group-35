becaa4a3f95f372eeaebe6f41b07a06f7d541cd6
Yarbework <yarbemohaz3517@gmail.com>
2025-08-03 17:36:43 +0300

    creating react vite with typescript and added necessary pages and components to develop front end

---
+++ FrontEnd/src/Pages/creatorProfile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function creatorProfile() {
+  return <div>creator Profile</div>;
+}
+
+export default creatorProfile;
---
+++ FrontEnd/src/Pages/signUp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function signUp() {
+  return <div>signup</div>;
+}
+
+export default signUp;
