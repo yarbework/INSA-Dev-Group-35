dbc78e02327b1a9b4d30047df28aca65e2509405
Firaol Kefeni <firaolkef@gmail.com>
2025-08-04 08:48:54 +0300

    Add Header component with basic routing using react-router-dom
    - Navigation links with Link
    - Setup BrowserRouter and Routes for Home and Login pages

--- FrontEnd/src/Components/Header/Header.tsx
+++ FrontEnd/src/Components/Header/Header.tsx
@@ -1,7 +1,42 @@
-import React from "react";
-import "./header.css";
-function Header() {
-  return <div>Header</div>;
+import "../../App.css";
+import { useNavigate } from "react-router-dom";
+
+function Button() {
+  const navigate = useNavigate();
+  return (
+    <button
+      onClick={() => navigate("Login")}
+      className="border-3 border-amber-200 px-5 py-1 rounded-xl hover:text-white hover:bg-amber-300 transition-colors duration-200"
+    >
+      Login
+    </button>
+  );
 }
 
-export default Header;
+export default function Header() {
+  return (
+    <>
+      <header className="w-full flex flex-row justify-between p-10 font-sans font-bold mb-0">
+        <h1>Logo</h1>
+
+        <nav className="flex gap-20">
+          <ul className="flex gap-10 text-gray-500">
+            <li className="hover:text-gray-700">
+              <a href="">Home</a>
+            </li>
+            <li className="hover:text-gray-700">
+              <a href="">Exams</a>
+            </li>
+            <li className="hover:text-gray-700">
+              <a href="">About us</a>
+            </li>
+          </ul>
+
+          <Button />
+        </nav>
+      </header>
+
+      <hr className="border-1 border-gray-400 mt-0" />
+    </>
+  );
+}
