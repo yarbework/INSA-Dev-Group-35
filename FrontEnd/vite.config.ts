c91634167fa43df3f9f5eeb65fc2c0ec209a74f9
Firaol Kefeni <firaolkef@gmail.com>
2025-08-05 16:24:53 +0300

    test for the header file and configuring the test setup

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import type { UserConfig } from 'vitest/config'  
+
+
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  
-}) 
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setupTests.ts'
+  },
+} as UserConfig) 
