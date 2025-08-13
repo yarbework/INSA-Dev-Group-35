1d92f0c7b00f116c501855c45a9c65779d625588
Bethelhem Alemayehu <alemayehubethelhem12@gmail.com>
2025-08-13 14:34:01 +0300

    Setup backend server with folder structure

---
+++ BackEnd/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
