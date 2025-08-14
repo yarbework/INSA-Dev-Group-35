bfc0b655e42ceaa855a7b42c11bea582d98bab9e
Yarbework <yarbemohaz3517@gmail.com>
2025-08-14 07:09:24 +0300

    Backend and DataBase integration and their structure

---
+++ BackEnd/models/Quiz.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const OptionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+});
+
+const QuestionSchema = new mongoose.Schema({
+  questionText: { type: String, required: true },
+  options: [OptionSchema],
+  CorrectAnswerIndex: { type: Number, required: true },
+});
+
+const QuizSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    subject: { type: String, required: true },
+    author: { type: String, default: "Admin" },
+    defficulty: {
+      type: String,
+      required: true,
+      enum: ["Easy", "Medium", "Hard"],
+      required: true,
+    },
+    timeLimit: { type: Number, required: true }, // this is in minutes
+    privacy: {
+      type: String,
+      enum: ["piblic", "private"],
+      default: "public",
+      required: true,
+    },
+    password: { type: String },
+    questions: [QuestionSchema],
+  },
+  { timestamps: true }
+);
+
+QuizSchema.virtual("questionCount").get(function () {
+  return this.questions.length;
+});
+
+module.exports = mongoose.model("Quiz", QuizSchema);
