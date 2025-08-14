a6d0eed5509cea89218e6c2eef1594d2bba3cb87
Yarbework <yarbemohaz3517@gmail.com>
2025-08-14 07:01:03 +0300

    adding exams page, create exam page and data base structure

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,29 +1,36 @@
 import "./App.css"; // css file
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { ExamsProvider } from "./context/ExamsContext";
+
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/signUp";
-import Exam from "./Pages/Exam";
+import Exam from "./Pages/ExamsPage";
 import About from "./Pages/About";
-import CreateQuiz from "./Pages/CreateQuiz";
-import NavLayout from "./Components/NavLayout";
+import CreateQuiz from "./Pages/CreateQuizPage";
+import NavLayout from "./components/NavLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* The following navLayout contains header and footer which is included to home, exam and about page */}
-        <Route path="/" element={<NavLayout />}>
-          <Route index element={<Home />} />
-          <Route path="exams" element={<Exam />} />
-          <Route path="about" element={<About />} />
-        </Route>
-        {/* The following statements are without header and footer */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/createQuiz" element={<CreateQuiz />} />
-      </Routes>
+      <AuthProvider>
+        <ExamsProvider>
+          <Routes>
+            {/* The following navLayout contains header and footer which is included to home, exam and about page */}
+            <Route path="/" element={<NavLayout />}>
+              <Route index element={<Home />} />
+              <Route path="exams" element={<Exam />} />
+              <Route path="about" element={<About />} />
+            </Route>
+            {/* The following statements are without header and footer */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/createQuiz" element={<CreateQuiz />} />
+          </Routes>
+        </ExamsProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
--- FrontEnd/src/Pages/CreateQuiz.tsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-function CreateQuiz() {
-  return <div>creating quiz</div>;
-}
-
-export default CreateQuiz;
