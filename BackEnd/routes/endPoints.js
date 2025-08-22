2ae02c7420d8bf9fb3ca8fd416d3cdcdcbb6f6c6
yarbework <yarbemohaz3517@gmail.com>
2025-08-22 18:17:58 +0300

    Profile page is created

--- BackEnd/routes/endPoints.js
+++ BackEnd/routes/endPoints.js
@@ -80,6 +80,63 @@ router.post("/logout", (req, res) => {
   res.json({ msg: "Logged out successfully" });
 });
 
+
+//===========================
+// GET USER PROFILE
+//===========================
+router.get('/profile', async(req, res)=>{
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({
+    msg: "Authentication token not found."
+  })
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // without password finding the user
+    const user = await User.findById(decoded.user.id).select("-password");
+
+    if (!user) return res.status(404).json({msg: "User not found."});
+    res.json(user)
+  }catch (err){
+    res.status(401).json({msg: "Invalid token."})
+  }
+})
+
+
+//===========================
+// UPDATE USER PROFILE
+//===========================
+router.put('/profile', async(req, res)=> {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({msg: "Authentication token not found."});
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    //fields to be destructured
+    const {username, school, grade, section} = req.body;
+
+    const user = await User.findById(decoded.user.id);
+    if (!user) return res.status(404).json({msg: "User not Found."});
+
+    //to update if they are requested
+    if(username) user.username = username;
+    if(school) user.school = school;
+    if(grade) user.grade = grade;
+    if(section) user.section = section;
+
+    const updatedUser = await user.save();
+
+    //creating user object to respond
+    const userToReturn = updatedUser.toObject();
+    delete userToReturn.password;
+
+    res.json(userToReturn);
+      }catch (err){
+        res.status(500).json({msg: "Server error during profile update."})
+      }
+
+})
+
+
 //===========================
 // GET CURRENT USER
 //===========================
@@ -109,20 +166,34 @@ router.put("/score", async (req, res) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const { subject, difficulty, score } = req.body;
+
+    //quizId to be sent from frontend
+    const { quizId, subject, difficulty, score } = req.body;
+    if(!quizId || !subject || score === undefined){
+      return res.status(400).json({msg: "Incomplete score data provided"})
+    }
+
     const user = await User.findById(decoded.user.id);
 
     if (!user) return res.status(404).json({ msg: "User not found" });
 
-    if (!user.scores) user.scores = [];
-    user.scores.push({ subject, difficulty, score });
-
-    await user.save();
-    res.json({ msg: "Score added successfully", scores: user.scores });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: "Unknown error" });
-  }
+    //cheching if score is aready exists
+    const existingScore = user.scores && user.scores.find((scoreEntry)=> scoreEntry.quizId.toString() === quizId)
+      
+    //if score doesn't exist. new score added
+    if(!existingScore){
+      user.scores.push({quizId, subject, difficulty, score})
+      await user.save();
+      res.json({msg: "Score for your fist attempt has been saved!", scores: user.scores})
+    }else{
+      //if score exist, inform the user
+      res.status(200).json({msg: "You have already completed this quiz. Only your first score is saved.", scores: user.scores})
+    }
+  }catch(error){
+      console.error("Error saving score:", error)
+      res.status(500).json({msg: "Server error while saving score."})
+    }
+    
 });
 
 module.exports = router;
